db804cfe98c3affb3edc7cc1bebcd33bb35d19c5
pxperfectmike <pxperfectmike@gmail.com>
2023-02-12 02:05:59 -0800

    add filterIt function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-export function forLoopUp(start, end, callback) {
+export function upLoop(start, end, callback) {
 	for (let i = start; i < end; i++) {
-		() => {
-			callback(i);
-		};
+		callback(i);
 	}
 }
 
-export function forLoopDown(start, end, callback) {
+export function downLoop(start, end, callback) {
 	for (let i = start; i > end; i--) {
-		() => {
-			callback(i);
-		};
+		callback(i);
 	}
 }
 
@@ -22,8 +18,55 @@ export function forEach(array, data, callback) {
 	});
 }
 
-export function map(array, callback) {
-	return array.map(() => {
-		callback;
-	});
+export function mapIt(array, callback) {
+	return array.map((element) => callback(element));
 }
+
+// invoking reduceIt without initialValue will default to 0
+function reduceIt(array, initialValue = 0) {
+	return array.reduce(
+		(accumulator, currentValue) => accumulator + currentValue,
+		initialValue
+	);
+}
+
+function filterIt(array, condition) {
+	let callback;
+	if (typeof condition === 'string') {
+		switch (condition) {
+			case 'even':
+				callback = (num) => num % 2 === 0;
+				break;
+			case 'odd':
+				callback = (num) => num % 2 !== 0;
+				break;
+			case 'greaterThan':
+				callback = (num) => num > condition.split('(')[1].split(')')[0];
+				break;
+			case 'lessThan':
+				callback = (num) => num > condition.split('(')[1].split(')')[0];
+				break;
+			case 'startsWith':
+				callback = (str) =>
+					str.startsWith(condition.split('(')[1].split(')')[0]);
+				break;
+			case 'length':
+				callback = (str) =>
+					str.length === condition.split('(')[1].split(')')[0];
+				break;
+			case 'contains':
+				callback = (str) => str.includes(condition.split('(')[1].split(')')[0]);
+				break;
+			case 'endsWith':
+				callback = (str) => str.endsWith(condition.split('(')[1].split(')')[0]);
+				break;
+			default:
+				throw new Error(`Invalid filter condition: ${condition}`);
+		}
+	} else {
+		callback = condition;
+	}
+	return array.filter(callback);
+}
+
+filterIt([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 'even'); // [2, 4, 6, 8, 10]
